8e9813c891199534179bc595ea710bd8f9109a5b
koglerm190074 <91053947+koglerm190074@users.noreply.github.com>
2022-05-16 13:11:21 +0200

    Resizing pictures

---
+++ Rezising.java
@@ -0,0 +1,77 @@
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+public class Rezising {
+    public static void resize(String inputImagePath,
+                              String outputImagePath, int scaledWidth, int scaledHeight)
+            throws IOException {
+        // reads input image
+        File inputFile = new File(inputImagePath);
+        BufferedImage inputImage = ImageIO.read(inputFile);
+
+        // creates output image
+        BufferedImage outputImage = new BufferedImage(scaledWidth,
+                scaledHeight, inputImage.getType());
+
+        // scales the input image to the output image
+        Graphics2D g2d = outputImage.createGraphics();
+        g2d.drawImage(inputImage, 0, 0, scaledWidth, scaledHeight, null);
+        g2d.dispose();
+
+        // extracts extension of output file
+        String formatName = outputImagePath.substring(outputImagePath
+                .lastIndexOf(".") + 1);
+
+        // writes to output file
+        ImageIO.write(outputImage, formatName, new File(outputImagePath));
+    }
+
+    /**
+     * Resizes an image by a percentage of original size (proportional).
+     * @param inputImagePath Path of the original image
+     * @param outputImagePath Path to save the resized image
+     * @param percent a double number specifies percentage of the output image
+     * over the input image.
+     * @throws IOException
+     */
+    public static void resize(String inputImagePath,
+                              String outputImagePath, double percent) throws IOException {
+        File inputFile = new File(inputImagePath);
+        BufferedImage inputImage = ImageIO.read(inputFile);
+        int scaledWidth = (int) (inputImage.getWidth() * percent);
+        int scaledHeight = (int) (inputImage.getHeight() * percent);
+        resize(inputImagePath, outputImagePath, scaledWidth, scaledHeight);
+    }
+
+    /**
+     * Test resizing images
+     */
+    public static void main(String[] args) {
+        String inputImagePath = "C:\\Users\\Mattikoglu\\Pictures\\benji.jpeg";
+        String outputImagePath1 = "C:\\Users\\Mattikoglu\\Pictures\\benji - Kopie.jpeg";
+        String outputImagePath2 = "C:\\Users\\Mattikoglu\\Pictures\\benji - Kopie(1).jpeg";
+        String outputImagePath3 = "C:\\Users\\Mattikoglu\\Pictures\\benji - Kopie(2).jpeg";
+
+        try {
+            // resize to a fixed width (not proportional)
+            int scaledWidth = 500;
+            int scaledHeight = 500;
+            resize(inputImagePath, outputImagePath1, scaledWidth, scaledHeight);
+
+            // resize smaller by 50%
+            double percent = 0.5;
+            resize(inputImagePath, outputImagePath2, percent);
+
+            // resize bigger by 50%
+            percent = 1.5;
+            resize(inputImagePath, outputImagePath3, percent);
+
+        } catch (IOException ex) {
+            System.out.println("Error resizing the image.");
+            ex.printStackTrace();
+        }
+    }
+}
